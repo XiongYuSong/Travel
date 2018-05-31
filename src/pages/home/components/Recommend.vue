72b1d717eb3bdfcb73ae934e544b061343bb31f8
XiongYuSong <937724308@qq.com>
2018-05-31 16:48:26 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title" v-text="item.title"></p>
+          <p class="item-desc" v-text="item.desc"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg',
+          title: '2222222222',
+          desc: '222222222222222222222222222222'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg',
+          title: '3333333333',
+          desc: '333333333333333333333333333333'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.recommend-title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eeeeee
+  text-indent 0.2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+.item-img
+  width 1.7rem
+  height 1.7rem
+  padding 0.1rem
+.item-info
+  flex 1
+  padding 0.1rem
+  min-width 0
+.item-title
+  line-height 0.54rem
+  font-size 0.32rem
+  ellipsis()
+.item-desc
+  line-height 0.4rem
+  color #cccccc
+  ellipsis()
+.item-button
+  line-height 0.44rem
+  margin-top 0.16rem
+  background #ff9300
+  padding 0 0.2rem
+  border-radius 0.06rem
+  color #ffffff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="weekend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title" v-text="item.title"></p>
+          <p class="item-desc" v-text="item.desc"></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=415293130,2419074865&fm=27&gp=0.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=415293130,2419074865&fm=27&gp=0.jpg',
+          title: '2222222222',
+          desc: '222222222222222222222222222222'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=415293130,2419074865&fm=27&gp=0.jpg',
+          title: '3333333333',
+          desc: '333333333333333333333333333333'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.weekend-title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eeeeee
+  text-indent 0.2rem
+.item-img-wrapper
+  height 0
+  overflow hidden
+  padding-bottom 33.9%
+.item-img
+  width 100%
+  height 158px
+.item-info
+  padding 0.1rem
+.item-title
+  line-height 0.54rem
+  font-size 0.32rem
+  ellipsis()
+.item-desc
+  line-height 0.4rem
+  color #cccccc
+  ellipsis()
+</style>
